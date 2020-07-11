b4366b63c3c9351bab86ad20765a412e2d749e6d
himzei <himzei@gmail.com>
2020-07-11 22:08:03 +0900

    tailwind css settings

--- users/models.py
+++ users/models.py
@@ -35,6 +35,13 @@ class User(AbstractUser):
         (CURRENCY_KRW, "KRW")
     )
 
+    LOGIN_EMAIL = "email"
+    LOGIN_GITHUB = "github"
+    LOGIN_KAKAO = "kakao"
+
+    LOGIN_CHOICES = ((LOGIN_EMAIL, "Email"), (LOGIN_GITHUB,
+                                              "Github"), (LOGIN_KAKAO, "Kakao"))
+
     avatar = models.ImageField(upload_to="avatars", null=True, blank=True)
     gender = models.CharField(choices=GENDER_CHOICES,
                               max_length=10, null=True, blank=True)
@@ -48,6 +55,8 @@ class User(AbstractUser):
     superhost = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
     email_secret = models.CharField(max_length=120, default="", blank=True)
+    login_method = models.CharField(
+        max_length=50, choices=LOGIN_CHOICES, default=LOGIN_EMAIL)
 
     def verify_email(self):
         if self.email_verified is False:
