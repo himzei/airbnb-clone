edbbe805d8ad7d52f777ebad6dc82ae420b39cca
himzei <himzei@gmail.com>
2020-07-17 12:39:58 +0900

    24.16 complete reviews

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from . import managers
 
 
 class TimeStampedModel(models.Model):
@@ -7,6 +8,7 @@ class TimeStampedModel(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = managers.CustomReservationManager()
 
     # abstract = True 는 데이터베이스에 등록되지 않는다
     class Meta:
--- reviews/models.py
+++ reviews/models.py
@@ -1,26 +1,32 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from core import models as core_models
 
 
 class Review(core_models.TimeStampedModel):
 
     review = models.TextField()
-    accruacy = models.IntegerField()
-    communication = models.IntegerField()
-    cleanliness = models.IntegerField()
-    location = models.IntegerField()
-    checkin = models.IntegerField()
-    value = models.IntegerField()
-    user = models.ForeignKey(
+    accruacy = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    communication = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    cleanliness = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    location = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    checkin = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    value = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    user= models.ForeignKey(
         "users.User", related_name="reviews", on_delete=models.CASCADE)
-    room = models.ForeignKey(
+    room= models.ForeignKey(
         "rooms.Room", related_name="reviews", on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.review} - {self.room.name}'
 
     def rating_average(self):
-        avg = (
+        avg= (
             + self.accruacy
             + self.communication
             + self.cleanliness
@@ -30,4 +36,7 @@ class Review(core_models.TimeStampedModel):
         ) / 6
         return round(avg, 2)
 
-    rating_average.short_description = "Avg."
+    rating_average.short_description= "Avg."
+
+    class Meta:
+        ordering= ('-created',)
